3085e5fbdb4304ba21fb45bba32d73518e5dd3a0
Sauraf Kumar <saurafsps7631@gmail.com>
2026-06-11 20:27:58 +0530

    Two sum

---
+++ TWO_SUM/Two Sum.py
@@ -0,0 +1,14 @@
+nums = [1,2,3,4,6]
+target = 6
+left = 0
+right = len(nums) - 1
+while left < right:
+    current_sum = nums[left] + nums[right]
+    if current_sum == target:
+        print([left, right])
+        break
+    elif current_sum < target:
+        left += 1
+    else:
+        right -= 1
+
