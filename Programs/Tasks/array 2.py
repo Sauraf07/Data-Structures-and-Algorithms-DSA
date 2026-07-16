36f3418415a3b0d8347763519ef79e03f621cf29
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-16 12:05:46 +0530

    Day 2 task

---
+++ Programs/Tasks/array 2.py
@@ -0,0 +1,27 @@
+'''Question 1
+Create an array of 5 numbers and print the first element.
+
+Question 2
+Print the last element of an array.
+
+Question 3
+Create an array of student names and print the third student.
+
+Question 4
+Replace the second element with 100.
+
+Question 5
+Replace the last fruit with "Mango".'''
+
+num = [10,20,30,40,50]
+print(num[0])
+
+print(num[-1])
+
+students = ['Alice', 'Bob', 'Charlie', 'David', 'Eva']
+print(students[2])
+
+num[1] = 100
+
+fruits = ['Apple', 'Banana', 'Orange', 'Grapes']
+fruits[-1] = 'Mango'
