33fc2098a9248af078070e4a6c41ecfe1c10f53c
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-16 12:02:40 +0530

    Day 1 task

---
+++ Programs/Tasks/Array 1 .py
@@ -0,0 +1,32 @@
+'''Coding
+Question 1
+Create an array of 10 numbers.
+
+Question 2
+Print the first element.
+
+Question 3
+Print the last element using negative indexing.
+
+Question 4
+Print the third element.
+
+Question 5
+Print the second last element.
+
+Question 6
+Create an array of five cities and print each city using indexing.'''
+
+num = [1,2,3,4,5,6,7,8,9,10]
+
+print(num[0]) # first element
+print(num[-1]) # last element using negative indexing
+print(num[2]) # third element
+print(num[-2])  # second last element
+
+cities = ['Indore','Bhopal','Pune','Mumbai','Delhi']
+print(cities[0]) # first city
+print(cities[1]) # second city
+print(cities[2]) # third city
+print(cities[3]) # fourth city
+print(cities[4]) # fifth city
