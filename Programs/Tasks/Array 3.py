33fabefd9707da7cc3b1e7dba01fef66f6a39970
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-16 20:54:54 +0530

    Day 3 task

---
+++ Programs/Tasks/Array 3.py
@@ -0,0 +1,19 @@
+'''Easy
+Print all elements.
+Print elements using while loop.
+Print elements in reverse order.
+Print index and value.
+Print even index elements.
+Print odd index elements.
+Medium
+Count total elements.
+Count even numbers.
+Count odd numbers.
+Count positive numbers.
+Count negative numbers.
+Count zeros.
+Challenge
+Print every alternate element.
+Print array from last to first.
+Traverse array without using for loop.'''
+
